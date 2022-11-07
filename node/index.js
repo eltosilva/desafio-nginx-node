b0aa061f4b21201c85c2ee094c536419432c8097
Elto Oliveira da Silva <petran.elto@gmail.com>
2022-11-07 18:51:06 -0300

    padronizando metodo get

--- node/index.js
+++ node/index.js
@@ -23,10 +23,27 @@ app.get('/clientes', (req, res) => {
   if(!connection)
     connection = getConnection()
 
-  connection.query('select * from clientes;',(erro, rows)=> {
+  let html = '<h1>Full Cycle Rocks!</h1>'
+
+  //- Lista de nomes cadastrada no banco de dados.`
+
+  connection.query('select * from clientes;', (erro, rows)=> {
     
-    if(erro) console.log(erro)
-    res.send(rows)
+    if(erro) throw erro
+
+    if(rows){
+      html += `<table>
+      <thead>
+        <tr><th>Id</th><th>Nome</th><th>Nasc</th></tr>
+      </thead>
+      <tbody>`
+      rows.forEach(row => {
+        html += `<tr><td>${row?.id}</td><td>${row?.nome}</td><td>${row?.nasc}</td></th>`
+      })
+      html += '</body></html>'
+    }
+
+    res.send(html)
   })
 })
 
